5af972025012c3e8b76f4e2183c9f0e860359d9b
Kevin Green <kevin@swedenunlimited.com>
2014-12-17 15:56:22 -0500

    adding some more dummy content

--- server/fixture.js
+++ server/fixture.js
@@ -1,8 +1,8 @@
 if (Meteor.users.find().count() == 0) {
    var users = [
-       {username:"normcore",name:"Normal User",email:"normal@site.com",roles:[], password: "normal3210"},
-       {username:"bookie",name:"Basic User",email:"normal@sit22e.com",roles:['bookie'], password: "bookie0"},
-       {username:process.env.ADMIN_USERNAME,name:process.env.ADMIN_NAME,email:process.env.ADMIN_EMAIL,roles:['admin'], password: process.env.ADMIN_PASSWORD}
+       {username:"normcore",name:"Normal User",email:"normal@site.com",roles:[], books: ['9780345508553'], password: "normal3210"},
+       {username:"bookie",name:"Basic User",email:"normal@sit22e.com",roles:['bookie'],books: ['9780345508553'], password: "bookie0"},
+       {username:process.env.ADMIN_USERNAME,books: ['9780345508553', '0765317389', '0060958324'],name:process.env.ADMIN_NAME,email:process.env.ADMIN_EMAIL,roles:['admin'], password: process.env.ADMIN_PASSWORD}
    ];
 
    _.each(users, function (user) {
@@ -10,7 +10,7 @@ if (Meteor.users.find().count() == 0) {
            username: user.username,
            email: user.email,
            password: user.password,
-           profile: { name: user.name }
+           profile: { name: user.name, books: user.books },
        });
 
        if (user.roles.length > 0) {
